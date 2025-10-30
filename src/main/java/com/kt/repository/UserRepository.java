2997d3e65a3ed305a9682f2de521fa786688c63b
goyounha11 <goyounha11@naver.com>
2025-10-30 11:03:51 +0900

    feat: user repository 회원가입 개발

--- src/main/java/com/kt/repository/UserRepository.java
+++ src/main/java/com/kt/repository/UserRepository.java
@@ -15,10 +15,34 @@ public class UserRepository {
 	private final JdbcTemplate jdbcTemplate;
 
 	public void save(User user) {
-		// 서비스에서 dto를 도메인(비지니스모델)으로 바꾼다음 전달
-		var sql = "INSERT INTO MEMBER (loginId, password, name, birthday) VALUES (?, ?, ?, ?)";
+		var sql = """
+			INSERT INTO MEMBER (
+			                    id,
+			                    loginId, 
+			                    password, 
+			                    name, 
+			                    birthday,
+			                    mobile,
+			                    email,
+			                    gender,
+			                    createdAt,
+			                    updatedAt
+			                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+			""";
 
-		jdbcTemplate.update(sql, user.getLoginId(), user.getPassword(), user.getName(), user.getBirthday());
+		jdbcTemplate.update(
+			sql,
+			user.getId(),
+			user.getLoginId(),
+			user.getPassword(),
+			user.getName(),
+			user.getBirthday(),
+			user.getMobile(),
+			user.getEmail(),
+			user.getGender().name(),
+			user.getCreatedAt(),
+			user.getUpdatedAt()
+		);
 	}
 
 	public Long selectMaxId() {
