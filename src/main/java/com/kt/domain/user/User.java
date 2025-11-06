347c8b6fed878921d6c10f5f5c9edf9838ace2ce
seungh22 <114427072+seungh22@users.noreply.github.com>
2025-11-07 04:44:37 +0900

    feat: introduce Order and OrderProduct entities with status management and relationships

--- src/main/java/com/kt/domain/user/User.java
+++ src/main/java/com/kt/domain/user/User.java
@@ -2,17 +2,21 @@ package com.kt.domain.user;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.kt.common.BaseEntity;
+import com.kt.domain.order.Order;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 // 1. domain과 entity를 분리해야
 // 2. 굳이? 같이쓰지 뭐
@@ -37,7 +41,10 @@ public class User extends BaseEntity {
 	private Gender gender;
 	private LocalDate birthday;
 
-	public User(String loginId, String password, String name, String mobile, String email, Gender gender,
+	@OneToMany(mappedBy = "user")
+	private List<Order> orders = new ArrayList<>();
+
+	public User(String loginId, String password, String name, String email, String mobile, Gender gender,
 		LocalDate birthday, LocalDateTime createdAt, LocalDateTime updatedAt) {
 		this.loginId = loginId;
 		this.password = password;
