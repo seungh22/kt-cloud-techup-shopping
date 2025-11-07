cbfb378fcb8d797f0e821c3808a1bf8770ebce5d
seungh22 <114427072+seungh22@users.noreply.github.com>
2025-11-07 09:18:10 +0900

    feat: introduce Receiver entity and embed it in Order for better structure

---
+++ src/main/java/com/kt/domain/order/Receiver.java
@@ -0,0 +1,19 @@
+package com.kt.domain.order;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Getter
+@Embeddable
+public class Receiver {
+	// receiver_name
+	@Column(name = "receiver_name")
+	private String name;
+	// receiver_address
+	@Column(name = "receiver_address")
+	private String address;
+	// receiver_mobile
+	@Column(name = "receiver_mobile")
+	private String mobile;
+}
