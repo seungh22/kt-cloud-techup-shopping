aaef7f9d2ac352c20843517f857a1a165ed23207
seungh22 <114427072+seungh22@users.noreply.github.com>
2025-11-21 14:49:43 +0900

    feat: implement distributed locking mechanism using Redisson and AOP for OrderService

---
+++ src/main/java/com/kt/aspect/AopTransactionManager.java
@@ -0,0 +1,8 @@
+package com.kt.aspect;
+
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public interface AopTransactionManager {
+	Object proceed(ProceedingJoinPoint joinPoint) throws Throwable;
+}
